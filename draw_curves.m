5ef485cc0aa6711759776c8c3eaa9b089b8849e6
johnzja <johnzja@qq.com>
2021-03-03 14:15:28 +0800

    beautify draw_curves

--- draw_curves.m
+++ draw_curves.m
@@ -1,6 +1,7 @@
 clear all;
 %% step1: Plot curves for sclf.
-figure(1);
+set(0,'defaultfigurecolor','w');
+figure('color',[1 1 1]);
 hold on;
 % Plot the performance of pure Parity-check-concatenated Polar codes.
 load data/compare_flip_with_others/bler_pcc12_t0.mat;
@@ -16,7 +17,7 @@ plot(Ebn0_arr, BLERs, 'rx-');
 
 grid on;
 set(gca, 'yscale', 'log');
-xlabel('Eb/n_0');
+xlabel('Eb/n_0(dB)');
 ylabel('BLER');
 
 % CRC8-PC4-polar code with flip(T=32).
@@ -29,8 +30,7 @@ load data/compare_flip_with_others/bler_crc8_casclf_t0_t32.mat;
 plot(Ebn0_arr, blers_crc8_casclf_t32, 'gx-');
 plot(Ebn0_arr, blers_crc8_cascl, 'gs-');
 
-
-legend('PCC12-SCL', 'CRC8-PCC8-SCL', 'CRC8-PCC8-SCLF(T=32)', 'CRC8-PCC4-SCLF(T=32)', 'CRC8-SCLF(T=32)', 'CRC8-SCL');
+legend('PCC12-SCL', 'CRC8-PC8-SCL', 'CRC8-PC8-SCLF(T=32)', 'CRC8-PC4-SCLF(T=32)', 'CRC8-SCLF(T=32)', 'CRC8-SCL');
 
 %% Plot2: plot the code performance with K_CRC and K_PCC.
 % 2-Dim plot.
